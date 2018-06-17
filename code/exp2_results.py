508f64b68dfbaa934d74523420346d721c7eb329
Jon Carr <jwcarr@users.noreply.github.com>
2018-06-17 19:16:13 +0100

    Regenerated stats CSV for Experiment 2
    Rewrote function to generate the CSV file for Experiment 2, which I must have removed at some point during tidying up. I then regenerated the CSV file, which now has greater decimal precision, resulting in a small changes in the decimals of the stats. So I've rerun the stats and these are now the canonical versions

--- code/exp2_results.py
+++ code/exp2_results.py
@@ -3,6 +3,22 @@ import il_visualize
 import colors
 import tools
 
+def generate_csv_for_stats(input_file, output_file, start_gen=1, end_gen=10):
+	dataset = tools.read_json_file(input_file)
+	csv = 'subject,chain,generation,expressivity,error,complexity,cost\n'
+	subject = 1
+	for chain_i, chain in enumerate(dataset['chains'], 1):
+		for gen_i in range(start_gen, end_gen+1):
+			generation = chain['generations'][gen_i]
+			expressivity = generation['prod_expressivity']
+			error = generation['prod_error']
+			complexity = generation['prod_complexity']
+			cost = generation['prod_cost']
+			csv += '%i,%i,%i,%i,%s,%s,%s\n' % (subject, chain_i, gen_i, expressivity, str(error), str(complexity), str(cost))
+			subject += 1
+	with open(output_file, mode='w') as file:
+		file.write(csv)
+
 def make_experiment_results_figure(figure_path):
 	experiment_results = il_results.load('../data/experiments/exp2_chains.json', start_gen=0, end_gen=10, method='prod')
 	il_results.make_figure([(experiment_results, 'Experiment', colors.black, colors.light_black, '-')],
@@ -24,6 +40,8 @@ def make_experiment_chains_figure(figure_path):
 	il_visualize.make_figure(data, figure_path, start_gen=0, end_gen=50, n_columns=14, rect_compress=True)
 
 ######################################################################
+
+# generate_csv_for_stats('../data/experiments/exp2_chains.json', '../data/experiments/exp2_stats_new.csv', 1, 10)
 
 # make_experiment_results_figure('../manuscript/figs/exp2_results.eps')
 # make_model_comparison_figure('../manuscript/figs/exp2_model_comparison.eps')
